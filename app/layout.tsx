265d6b4802b5c4df94ce070141b670a3d25da42a
peacefulrevolution7 <p.sauthoff@web.de>
2026-04-16 15:00:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
-import ‘./globals.css’
-import type { Metadata } from ‘next’
-import { Inter } from ‘next/font/google’
-import Header from ‘@/components/Header’
-import Footer from ‘@/components/Footer’
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: [‘latin’] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-title: ‘Weltfriedensabkommen’,
-description: ‘Ein Friedensvertrag zwischen Weltbürgern’,
+  title: 'Weltfriedensabkommen',
+  description: 'Ein Friedensvertrag zwischen Weltbürgern',
 }
 
 export default function RootLayout({
-children,
+  children,
 }: {
-children: React.ReactNode
+  children: React.ReactNode
 }) {
-return (
-<html lang="de">
-<body className={inter.className}>
-<Header />
-<main className="min-h-screen">
-{children}
-</main>
-<Footer />
-</body>
-</html>
-)
-}
+  return (
+    <html lang="de">
+      <body className={inter.className}>
+        <Header />
+        <main className="min-h-screen">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
